c49040bd43e38fc937eab6b60c5865b15207148c
Daniel <Dans@Daniels-MacBook-Pro.local>
2016-11-06 09:04:14 -0500

    test

--- gui.py
+++ gui.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
 try:
     # for Python2
-    from Tkinter import * 
+    import Tkinter as tkinter
+    import ttk as ttk
 except ImportError:
     # for Python3
     from tkinter import *
+    import ttk as ttk
 
-class App(TK.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-myapp = App()
+import sys
 
-myapp.master.title("My Do-Nothing Application")
-myapp.master.maxsize(1000, 400)
-myapp.mainloop()
+x=tkinter.Tk(screenName=None, baseName=None, className='Tk', useTk=1)
+y=ttk.Combobox()
+y.set(8)
+y.current(newindex=1)
+x.mainloop()
+
+'''
+listen thread
+attack thread
+interface thread
+
+connection dictionary
+with lock
+
+attack qeueue
+with lock
+'''
